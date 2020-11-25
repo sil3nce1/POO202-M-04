7b325bc464868816d2cf5ad11c1df22676d75813
Matheus Bailer Demer <66788280+MatthewBailerDemmer@users.noreply.github.com>
2020-11-25 19:45:12 -0300

    Add files via upload
    atualização enigma1

--- Enigma1.java
+++ Enigma1.java
@@ -1,4 +1,4 @@
-package Intregavel2;
+package Modulo_Completo;
 
 import javax.swing.JPanel;
 import javax.swing.JLabel;
@@ -12,6 +12,9 @@ import javax.swing.JRadioButtonMenuItem;
 import java.awt.event.ActionListener;
 import java.awt.event.ActionEvent;
 import java.awt.Button;
+import javax.swing.ButtonGroup;
+import javax.swing.JButton;
+import javax.swing.ImageIcon;
 
 public class Enigma1 extends JPanel {
 	private JRadioButton rdbtnVerdadeiro;
@@ -35,62 +38,60 @@ public class Enigma1 extends JPanel {
 		JLabel lblEnigna1 = new JLabel("3\t+ 4 = 7 se, e somente se, 7 n\u00E3o \u00E9 um n\u00FAmero primo.");
 		lblEnigna1.setForeground(new Color(255, 0, 0));
 		lblEnigna1.setFont(new Font("Baskerville Old Face", Font.BOLD, 7));
-		lblEnigna1.setBounds(149, 35, 155, 20);
+		lblEnigna1.setBounds(56, 106, 155, 20);
 		add(lblEnigna1);
 		
 		rdbtnVerdadeiro = new JRadioButton("Verdadeiro");
-		rdbtnVerdadeiro.addActionListener(new ActionListener() {
-			public void actionPerformed(ActionEvent arg0) {
-				if (rdbtnVerdadeiro.isSelected())
-				   rdbtnFalso.setSelected(false);
-				   rdbtnNãoFazSentido.setSelected(false);
-			}
-		});
-		rdbtnVerdadeiro.addFocusListener(new FocusAdapter() {
-			@Override
-			public void focusLost(FocusEvent arg0) {
-			}
-		});
 		rdbtnVerdadeiro.setForeground(new Color(0, 0, 139));
 		rdbtnVerdadeiro.setFont(new Font("Baskerville Old Face", Font.BOLD, 11));
-		rdbtnVerdadeiro.setBounds(84, 89, 155, 29);
+		rdbtnVerdadeiro.setBounds(46, 73, 155, 29);
 		add(rdbtnVerdadeiro);
 		
 		rdbtnFalso_1 = new JRadioButton("Falso");
-		rdbtnFalso_1.addActionListener(new ActionListener() {
-			public void actionPerformed(ActionEvent e) {
-				if (rdbtnFalso.isSelected())
-					   rdbtnVerdadeiro.setSelected(false);
-					   rdbtnNãoFazSentido.setSelected(false);
-				
-			}
-			
-		});
 		rdbtnFalso_1.setForeground(new Color(0, 255, 0));
 		rdbtnFalso_1.setFont(new Font("Sitka Banner", Font.BOLD, 23));
-		rdbtnFalso_1.setBounds(319, 132, 155, 29);
+		rdbtnFalso_1.setBounds(262, 101, 155, 29);
 		add(rdbtnFalso_1);
 		
 		rdbtnNãoFazSentido_1 = new JRadioButton("N\u00E3o faz sentido");
-		rdbtnNãoFazSentido_1.addActionListener(new ActionListener() {
-			public void actionPerformed(ActionEvent e) {
-				if (rdbtnNãoFazSentido.isSelected())
-					   rdbtnFalso.setSelected(false);
-					   rdbtnVerdadeiro.setSelected(false);
-			}
-		});
 		rdbtnNãoFazSentido_1.addFocusListener(new FocusAdapter() {
 			@Override
 			public void focusLost(FocusEvent arg0) {
 			}
 		});
 		rdbtnNãoFazSentido_1.setFont(new Font("Stencil", Font.PLAIN, 23));
-		rdbtnNãoFazSentido_1.setBounds(46, 181, 249, 29);
+		rdbtnNãoFazSentido_1.setBounds(24, 146, 249, 29);
 		add(rdbtnNãoFazSentido_1);
 		
-		Button button = new Button("Desativar");
-		button.setBounds(26, 228, 438, 44);
-		add(button);
+		ButtonGroup bg = new ButtonGroup();
+		bg.add(rdbtnVerdadeiro);
+		bg.add(rdbtnFalso_1);
+		bg.add(rdbtnNãoFazSentido_1);
+		
+		JLabel lblNewLabel = new JLabel("Armado");
+		lblNewLabel.setBounds(330, 24, 48, 14);
+		add(lblNewLabel);
+		
+		
+		JButton btnNewButton = new JButton("Desarmar");
+		btnNewButton.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+				ReasoningLogic enigma1 = new ReasoningLogic();
+				enigma1.IncrementActivations();
+				if(rdbtnFalso_1.isSelected()) {
+					enigma1.IncrementRightAnsweres();
+					lblNewLabel.setText("Desarmado");
+				}else {
+					enigma1.IncrementWrongAnsweres();
+				}
+				
+			}
+		});
+		btnNewButton.setIcon(new ImageIcon(Enigma1.class.getResource("/Imagens/MicrosoftTeams-image.png")));
+		btnNewButton.setBounds(56, 214, 316, 54);
+		add(btnNewButton);
+		
+		
 
 	}
 }
